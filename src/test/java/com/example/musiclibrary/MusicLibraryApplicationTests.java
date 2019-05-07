9ce9e25e383044f014473ce26a1c2aab7895e2a2
Pavel Ponomarenko <p.ponomarenko@krtech.ru>
2019-05-07 18:07:48 +0300

    start

---
+++ src/test/java/com/example/musiclibrary/MusicLibraryApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.musiclibrary;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MusicLibraryApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
